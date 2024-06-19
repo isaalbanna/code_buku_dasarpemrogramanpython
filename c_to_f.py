dce681f6bfdf9ea962a7c60855691dab43177fc2
isaalbanna_itats <61767893+isaalbanna@users.noreply.github.com>
2024-06-19 22:36:42 +0700

    Create c_to_f.py

---
+++ c_to_f.py
@@ -0,0 +1,8 @@
+def konversi_ctof(nilai_c):
+    nilai_f=((9/5)*nilai_c)+32
+    return nilai_f
+
+c=23
+f=konversi_ctof(c)
+print("Nilai awal Suhu Celcius=",c)
+print("Nilai konversi menjadi Suhu Fahrenheit=",f)
